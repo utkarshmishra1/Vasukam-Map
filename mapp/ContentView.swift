33464d2ba2c604da12a0d3a1e49c05b72c22fda6
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-07-30 11:09:10 +0530

    Initial Commit

---
+++ mapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mapp
+//
+//  Created by utkarsh mishra on 30/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mapp/mappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mappApp.swift
+//  mapp
+//
+//  Created by utkarsh mishra on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct mappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
